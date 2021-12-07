c8f2ec16a05b706aebfbc3877124a7b7241bbe6e
Ville Misaki <ville@misaki.fi>
2021-12-07 21:30:13 +0900

    Initial Commit

---
+++ calc-sb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calc-sb
+//
+//  Created by Ville Misaki on 21-12-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
